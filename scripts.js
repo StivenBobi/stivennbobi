b13bcacc9a68304c72f4488652d36e352c176bc6
StivenBobi <125212544+StivenBobi@users.noreply.github.com>
2023-02-18 21:08:15 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.body.className = 'fade';
+document.addEventListener("DOMContentLoaded", () => {
+    window.setTimeout(function() {
+      document.body.classList.remove('fade');
+    }, 230);
+  });
